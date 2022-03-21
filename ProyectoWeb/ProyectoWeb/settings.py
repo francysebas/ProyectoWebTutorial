579cd8605e03cf636af40dfc9532a55e045791e9
francysebas <francyn@unicauca.edu.co>
2022-03-21 13:25:30 -0500

    modelos, administrador, imagenes, observar iamagenes, otra aplicsacion, herencia de plantillas

--- ProyectoWeb/ProyectoWeb/settings.py
+++ ProyectoWeb/ProyectoWeb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ProyectoWebApp',
+    'AppServicios',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -116,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- ProyectoWeb/ProyectoWebApp/views.py
+++ ProyectoWeb/ProyectoWebApp/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render, HttpResponse
 
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Inicio...")
+    # return HttpResponse("Inicio...")
+    return render(request, "ProyectoWebApp/home.html/")
+
 
 def servicio(request):
-    return HttpResponse("Servicios...")
+    return render(request, "ProyectoWebApp/servicio.html/")
+
 
 def tienda(request):
-    return HttpResponse("Tienda")
+    return render(request, "ProyectoWebApp/tienda.html/")
+
 
 def blog(request):
-    return HttpResponse("Blog")
+    return render(request, "ProyectoWebApp/blog.html/")
+
 
 def contacto(request):
-    return HttpResponse("Contacto")
+    return render(request, "ProyectoWebApp/contacto.html/")
