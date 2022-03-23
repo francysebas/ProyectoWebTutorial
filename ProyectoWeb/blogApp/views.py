e0dd1b6b4606f6df6a98b464e6a73cf7737a6dc4
francysebas <31090790+francysebas@users.noreply.github.com>
2022-03-23 08:34:54 -0500

    slice links de categorias

--- ProyectoWeb/blogApp/views.py
+++ ProyectoWeb/blogApp/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from blogApp.models import Post
+from django.shortcuts import render
+from blogApp.models import Post, Categoria
 
 
 # Create your views here.
 
-
 def blog(request):
     posts = Post.objects.all()
     return render(request, "blogApp/blog.html", {"posts": posts})
+
+
+# muestra en otra vista solo los filtros de las categorias
+def categoria(request, categoria_id):
+    categori = Categoria.objects.get(id=categoria_id)
+    posts = Post.objects.filter(categorias=categori)
+
+    return render(request, "blogApp/categoria.html", {'categoria': categori, "posts": posts})
