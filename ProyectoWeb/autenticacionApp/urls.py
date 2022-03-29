2e73fe96a192413bbf2b6e5640b614a92ae30c0b
francysebas <31090790+francysebas@users.noreply.github.com>
2022-03-29 18:06:31 -0500

    autenticación form

--- ProyectoWeb/autenticacionApp/urls.py
+++ ProyectoWeb/autenticacionApp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import VRegistro
 
 urlpatterns = [
 
-    path('', views.autenticacion, name="Autenticacion"),
+    path('', VRegistro.as_view(), name="Autenticacion"),
 
 ]
--- ProyectoWeb/autenticacionApp/views.py
+++ ProyectoWeb/autenticacionApp/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from  django.contrib.auth.forms import UserCreationForm
 # Create your views here.
-
+'''
 def autenticacion(request):
-    total = 0
-    return total
+    return render(request, "registro/registro.html")
+'''
+
+class VRegistro(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, "registro/registro.html", {"form": form})
+
+    def post(self, request):
+        pass
+
+
+
